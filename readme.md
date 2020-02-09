9840c9b7eea8e70f194551e3d30e9b0f657d0851
nobuo <nobuosato@gmail.com>
2020-02-09 11:45:28 -0500

    update

--- readme.md
+++ readme.md
@@ -3,25 +3,33 @@
 
 ## Installation 
 
+### prepare a jam python enviroment
+
 - Get anaconda3 and install it in your system
 
 - Create an enviroment 
 
-  ```conda create --name jam python=2.7```
+  ```conda create --name jam python=2.7'''
 
 - Activate the enviroment 
 
-  ```conda activate jam```
+  ```conda activate jam'''
 
-- Install subprocess32
+- Install subprocess32 
 
-  ```conda install subprocess32```
+  ```conda install subprocess32'''
+
+- Install all the dependencies 
+
+  ```pip install -r dependencies'''
 
 
-
-
+### clone the repo
 
 git clone --recursive git@github.com:JeffersonLab/jam3dlib.git 
+
+
+
 
 
 
--- tmd.py
+++ tmd.py
@@ -16,20 +16,20 @@ class TMD:
         self.par=self.jar['par']
         self.nrep=len(self.par)
 
-    def eval(self,x,Q2,kT,had,dist,irep):
+    def eval(self,x,Q2,kT,had,dist,irep,icol=False):
 
         self.parman.set_new_params(self.par[irep],initial=True)
 
         if  dist=='pdf' and had=='p':
-            return conf['pdf'].get_tmd(x,Q2,kT,'p','pdf')  
+            return conf['pdf'].get_tmd(x,Q2,kT,'p','pdf',icol=icol)  
         elif dist=='ff' and had=='pi+': 
-            return conf['ffpi'].get_tmd(x,Q2,kT,'pi','ffpi')  
+            return conf['ffpi'].get_tmd(x,Q2,kT,'pi','ffpi',icol=icol)  
         elif dist=='sivers' and had=='p':
-            return conf['sivers'].get_tmd(x,Q2,kT,'p','sivers')  
+            return conf['sivers'].get_tmd(x,Q2,kT,'p','sivers',icol=icol)  
         elif dist=='transversity' and had=='p':
-           return conf['transversity'].get_tmd(x,Q2,kT,'p','transversity')  
+           return conf['transversity'].get_tmd(x,Q2,kT,'p','transversity',icol=icol)  
         elif dist=='collinspi' and had=='pi':
-            return conf['collinspi'].get_tmd(x,Q2,kT,'pi','collinspi')  
+            return conf['collinspi'].get_tmd(x,Q2,kT,'pi','collinspi',icol=icol)  
         else:
             print('dist and had not available')
             sys.exit()
