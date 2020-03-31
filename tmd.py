4fe2203b0d10ada59f3def6cfc3cad2ecc2aa43c
Alexei Prokudin <prokudin@jlab.org>
2020-03-31 10:40:07 -0400

    Structure functions and asymmetries

--- tmd.py
+++ tmd.py
@@ -3,6 +3,9 @@ import numpy as np
 from tools.config  import load_config, conf
 from tools.tools   import load
 from fitlib.resman import RESMAN
+from obslib.sidis import upol0 as upol
+from obslib.sidis import sivers0 as sivers
+from obslib.sidis import collins0 as collins
 
 class TMD:
 
@@ -33,3 +36,51 @@ class TMD:
         else:
             print('dist and had not available')
             sys.exit()
+
+    def eval_stfunc(self,stfunc,x,z,Q2,pT,tar,had,irep,icol=False):
+        
+        self.parman.set_new_params(self.par[irep],initial=True)
+        
+        if stfunc=='FUU':
+            return upol.get_FUU(x,z,Q2,pT,tar,had)
+        elif stfunc=='FUTSiv':
+            return sivers.get_FUT(x,z,Q2,pT,tar,had)
+        elif stfunc=='FUTCol':
+            return collins.get_FUT(x,z,Q2,pT,tar,had)
+        else:
+            print(stfunc,'is not available')
+            sys.exit()
+
+    def eval_asymmetry(self,asymmetry,x,z,Q2,pT,tar,had,irep,icol=False):
+        
+        self.parman.set_new_params(self.par[irep],initial=True)
+        
+        if asymmetry=='Siv': # sin(phi_s-phi_h)
+            return self.eval_stfunc('FUTSiv',x,z,Q2,pT,tar,had,irep,icol)/self.eval_stfunc('FUU',x,z,Q2,pT,tar,had,irep,icol)
+        elif stfunc=='Col':  # sin(phi_s+phi_h) NO DEPOLARIZATION FACTOR ADDED
+            return self.eval_stfunc('FUTSiv',x,z,Q2,pT,tar,had,irep,icol)/self.eval_stfunc('FUU',x,z,Q2,pT,tar,had,irep,icol)
+        else:
+            print(stfunc,'is not available')
+            sys.exit()
+             
+            
+if __name__ == '__main__':
+
+
+    x = 0.25
+    z = 0.5
+    Q2 = 2.4
+    pT = 0.3
+    tar = 'p'
+    had = 'pi+'
+    stfunc='FUTCol'
+    
+    tag='JAM3D_2020' 
+    tmd=TMD(tag)
+    
+    print(tmd.eval_stfunc(stfunc, x, z, Q2, pT, tar, had, 0, icol=False)) 
+    print(tmd.eval_asymmetry('Siv', x, z, Q2, pT, tar, had, 0, icol=False)) 
+
+
+
+     
