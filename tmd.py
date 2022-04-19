4e48b97456be6a613ddbba7b26276e3b8c3327e5
pitonyak25 <pitonyak@lvc.edu>
2022-04-18 22:38:53 -0400

    jam22 updates

--- tmd.py
+++ tmd.py
@@ -6,6 +6,7 @@ from fitlib.resman import RESMAN
 from obslib.sidis import upol0 as upol
 from obslib.sidis import sivers0 as sivers
 from obslib.sidis import collins0 as collins
+from obslib.sidis import aUTsPs0 as sinphiS
 from obslib.AN_pp import AN_theory0 as AN_theory
 
 class TMD:
@@ -15,7 +16,7 @@ class TMD:
         load_config('data/input-%s.py'%tag)
         resman=RESMAN(nworkers=1,parallel=False,datasets=False)
         self.parman=resman.parman
-        self.jar=load('data/jar-%s.po'%tag)
+        self.jar=np.load('data/jar-%s.npy'%tag,allow_pickle=True,encoding = 'latin1').item()
         self.parman.order=self.jar['order']
         self.par=self.jar['par']
         self.nrep=len(self.par)
@@ -31,9 +32,11 @@ class TMD:
         elif dist=='sivers' and had=='p':
             return conf['sivers'].get_tmd(x,Q2,kT,'p','sivers',icol=icol,deriv=False)
         elif dist=='transversity' and had=='p':
-           return conf['transversity'].get_tmd(x,Q2,kT,'p','transversity',icol=icol,deriv=False)
+            return conf['transversity'].get_tmd(x,Q2,kT,'p','transversity',icol=icol,deriv=False)
         elif dist=='collinspi' and had=='pi':
             return conf['collinspi'].get_tmd(x,Q2,kT,'pi','collinspi',icol=icol,deriv=False)
+        elif dist=='Htildepi' and had=='pi':
+            return conf['Htildepi'].get_tmd(x,Q2,kT,'pi','Htildepi',icol=True,deriv=False)
         else:
             print('dist and had not available')
             sys.exit()
@@ -48,6 +51,8 @@ class TMD:
             return sivers.get_FUT(x,z,Q2,pT,tar,had)
         elif stfunc=='FUTCol':
             return collins.get_FUT(x,z,Q2,pT,tar,had)
+        elif stfunc=='FUTsinphiS':
+            return sinphiS.get_FX(x,z,Q2,None,tar,had)
         else:
             print(stfunc,'is not available')
             sys.exit()
@@ -60,6 +65,8 @@ class TMD:
             return self.eval_stfunc('FUTSiv',x,z,Q2,pT,tar,had,irep,icol)/self.eval_stfunc('FUU',x,z,Q2,pT,tar,had,irep,icol)
         elif asym=='Col':  # sin(phi_s+phi_h) NO DEPOLARIZATION FACTOR ADDED
             return self.eval_stfunc('FUTCol',x,z,Q2,pT,tar,had,irep,icol)/self.eval_stfunc('FUU',x,z,Q2,pT,tar,had,irep,icol)
+        elif asym=='sinphiS':
+            return self.eval_stfunc('FUTsinphiS',x,z,Q2,None,tar,had,irep,icol)/self.eval_stfunc('FUU',x,z,Q2,None,tar,had,irep,icol)
         else:
             print(stfunc,'is not available')
             sys.exit()
@@ -85,8 +92,9 @@ if __name__ == '__main__':
     had = 'pi+'
     stfunc='FUTCol'
 
-    tag='JAM3D_2020'
+    tag='JAM3D_2022'
     tmd=TMD(tag)
 
     print(tmd.eval_stfunc(stfunc, x, z, Q2, pT, tar, had, 0, icol=False))
     print(tmd.eval_asymmetry('Siv', x, z, Q2, pT, tar, had, 0, icol=False))
+    print(tmd.eval_asymmetry('sinphiS', x, z, Q2, None, tar, had, 0, icol=False))
